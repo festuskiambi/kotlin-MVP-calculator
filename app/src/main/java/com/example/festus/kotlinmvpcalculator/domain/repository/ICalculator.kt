983d606b426d8187d98e6808d248b2ac22609a51
festus <kiamfestoh@gmail.com>
2018-11-05 22:22:11 +0300

    create calculator interface

--- app/src/main/java/com/example/festus/kotlinmvpcalculator/domain/repository/ICalculator.kt
+++ app/src/main/java/com/example/festus/kotlinmvpcalculator/domain/repository/ICalculator.kt
@@ -1,5 +1,13 @@
 package com.example.festus.kotlinmvpcalculator.domain.repository
 
+import com.example.festus.kotlinmvpcalculator.data.datamodel.ExpressionDataModel
+import io.reactivex.Flowable
+
 /**
  * Created by Festus Kiambi on 11/5/18.
  */
+
+interface ICalculator {
+
+    fun evaluateExpression(expression: String): Flowable<ExpressionDataModel>
+}
